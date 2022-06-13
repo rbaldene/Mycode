84045599c0fbb1e1dd49a190cb4dbb76eda96e83
Mona Lisa <rbaldene@cox.net.github.com>
2022-06-13 22:23:51 +0000

    mixed lists

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -13,4 +13,8 @@ print("The second item in the list (port): " + str(my_list[1]) )
 print("The last item in the list (state): " + my_list[2] )
 
 my_list2 = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
-print(my_list2[3]", and " + my_list2 [4])
+print("IP Addresses:", my_list2[3], "and " + my_list2 [4])
+
+new_list = ["blue", "green", "yellow"]
+print(f"Misc data: {my_list[1]}, and {new_list[2]}") 
+print(f"More misc data: {my_list[2]}, {new_list[0]}, and {my_list[0]}")
